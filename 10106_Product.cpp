222ee9e01787395fa3adc6f86607483efac802dc
Anisur Rahaman <60539253+Anisur-Rahaman00@users.noreply.github.com>
2022-03-03 22:40:16 +0600

    Create 10106_Product.cpp

---
+++ 10106_Product.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+
+#define I_O ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+//~~~~~~~~~~~~ Sort Form Of Long~~~~~~~~~~~//
+#define ll long long int
+#define lls int64_t
+#define ld long double
+#define db double
+#define ull unsigned long long int
+
+//~~~~~~~~~~~~~~Pair~~~~~~~~~~~~~~~~~~//
+#define pii pair<int, int>
+#define pll pair<lls, lls>
+#define pdd pair<db,db>
+#define psi pair<string,int>
+#define vi vector<int>
+#define vl vector<lls>
+#define vd vector<db>
+#define vs vector<string>
+#define vb vector<bool>
+#define vpi vector<pii>
+#define vpl vector<pll>
+#define vpd vector<pdd>
+#define vpsi vector<psi>
+#define vvi vector<vector<int>>
+
+//~~~~~~~~~~~~~~Shorts~~~~~~~~~~~~~~~~~//
+#define ff first
+#define ss second
+#define al(v) v.begin(),v.end()
+#define alr(v) v.rbegin(), v.rend()
+#define srt(v) sort(al(v))
+#define srtr(v) sort(al(v), greater<int>());
+#define len(x) ((int)(x).size())
+#define rev(v) reverse(al(v))
+#define btcnt(n) __builtin_popcount(n)
+#define acl(v, n) accumulate(al(v), n)
+#define Lrt(s, c) rotate(s.begin(), s.begin() + c, s.end())
+#define Rrt(s, c) rotate(s.begin(), s.begin() + s.size() - c, s.end())
+#define lb(v, kk) lower_bound(al(v), kk) - v.begin()
+#define ub(v, kk) upper_bound(al(v), kk) - v.begin()
+#define tpu(str) transform(al(str), str.begin(), ::toupper)
+#define tpl(str) transform(al(str), str.begin(), ::tolower)
+#define cignr cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define mxv(v) *max_element(al(v))
+#define mnv(v) *min_element(al(v))
+
+const int MOD = 1e9 + 7;
+const lls INF = 2e18;
+const int mxn = 2e9 + 9;
+const int mxd = 2e5 + 5;
+const int mxa = 2e6 + 5;
+
+//~~~~~~~~~~~~~~~~~~Function~~~~~~~~~~~~~~~~~~~~//
+lls gcd(lls a, lls b){ if(b == 0LL) return a; return gcd(b, a % b); }
+lls lcm(lls a, lls b){ return (a / gcd(a, b) * b); }
+lls maxll(lls x, lls y){ return x > y ? x : y; }
+lls minll(lls x, lls y){ return x < y ? x : y; }
+
+//~~~~~~~~~~~~~~~Loops and Short~~~~~~~~~~~~~~~~//
+
+#define PI acos(-1)
+#define N '\n'
+#define fopen freopen("input.txt", "r", stdin);
+#define rep(i, n) for(lls i = 0; (lls)i < n; i++)
+#define test_case() int T; T = 1; while(T--)
+
+using namespace std;
+
+// ===================================~~~~~~ SOLUTION STARTS HERE ~~~~~~=================================== //
+
+//int a, b, c, d, e, res = 0, ans = 0, prod = 1, n, m, cnt = 0, k, l, r, s, t, x, y, f, i, j, p;
+
+string multiply(string num1, string num2)
+{
+    int len1 = num1.size();
+    int len2 = num2.size();
+    if (len1 == 0 || len2 == 0)
+    return "0";
+ 
+    // will keep the result number in vector
+    // in reverse order
+    vector<int> result(len1 + len2, 0);
+ 
+    // Below two indexes are used to find positions
+    // in result.
+    int i_n1 = 0;
+    int i_n2 = 0;
+     
+    // Go from right to left in num1
+    for (int i=len1-1; i>=0; i--)
+    {
+        int carry = 0;
+        int n1 = num1[i] - '0';
+ 
+        // To shift position to left after every
+        // multiplication of a digit in num2
+        i_n2 = 0;
+         
+        // Go from right to left in num2            
+        for (int j=len2-1; j>=0; j--)
+        {
+            // Take current digit of second number
+            int n2 = num2[j] - '0';
+ 
+            // Multiply with current digit of first number
+            // and add result to previously stored result
+            // at current position.
+            int sum = n1*n2 + result[i_n1 + i_n2] + carry;
+ 
+            // Carry for next iteration
+            carry = sum/10;
+ 
+            // Store result
+            result[i_n1 + i_n2] = sum % 10;
+ 
+            i_n2++;
+        }
+ 
+        // store carry in next cell
+        if (carry > 0)
+            result[i_n1 + i_n2] += carry;
+ 
+        // To shift position to left after every
+        // multiplication of a digit in num1.
+        i_n1++;
+    }
+ 
+    // ignore '0's from the right
+    int i = result.size() - 1;
+    while (i>=0 && result[i] == 0)
+    i--;
+ 
+    // If all were '0's - means either both or
+    // one of num1 or num2 were '0'
+    if (i == -1)
+    return "0";
+ 
+    // generate the result string
+    string s = "";
+     
+    while (i >= 0)
+        s += std::to_string(result[i--]);
+ 
+    return s;
+}
+
+void Run_Case() 
+{
+   string s, t;
+   while (cin >> s >> t) {
+      cout << multiply(s, t) << N;
+   }
+}
+
+int main()
+{
+   I_O
+   test_case()
+   {
+      Run_Case();
+   }
+
+   return 0;
+}
