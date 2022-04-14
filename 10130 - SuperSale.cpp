c495abd136ead01755de02a2cc2075757cb74a24
Anisur Rahaman <60539253+anis017@users.noreply.github.com>
2022-04-14 12:40:46 +0600

    Update 10130 - SuperSale.cpp

--- 10130 - SuperSale.cpp
+++ 10130 - SuperSale.cpp
@@ -90,23 +90,19 @@ int dy[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
 
 int knapSack(int W, int wt[], int val[], int n)
 {
-    int i, w;
-    vector<vector<int>> K(n + 1, vector<int>(W + 1));
-  
-    for(i = 0; i <= n; i++)
-    {
-        for(w = 0; w <= W; w++)
-        {
-            if (i == 0 || w == 0)
-                K[i][w] = 0;
-            else if (wt[i - 1] <= w)
-                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
-            else
-                K[i][w] = K[i - 1][w];
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1));
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (!i or !j) dp[i][j] = 0;
+            else if (wt[i - 1] <= j) {
+                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+            } 
+            else dp[i][j] = dp[i - 1][j];
         }
     }
 
-    return K[n][W];
+    return dp[n][W];
 }
 
 void Run_Case()
