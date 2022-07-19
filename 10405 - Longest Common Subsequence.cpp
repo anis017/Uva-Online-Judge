9bf4f2f256ee70c30eccc2f35c55f3375da2cce8
anis_bns <60539253+anis-bns-017@users.noreply.github.com>
2022-07-19 11:14:23 +0600

    Update 10405 - Longest Common Subsequence.cpp

--- 10405 - Longest Common Subsequence.cpp
+++ 10405 - Longest Common Subsequence.cpp
@@ -83,7 +83,7 @@ int lcs(string s, string t, int n, int m) {
          else if (s[i - 1] == t[j - 1]) {
             dp[i][j] = 1 + dp[i - 1][j -1];
          }
-         else dp[i][j] = max(dp[i - 1][j], dp[i][j -1]);
+         else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
       }
    }
 
